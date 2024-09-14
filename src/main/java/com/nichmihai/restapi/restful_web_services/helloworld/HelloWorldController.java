8d4018153ccc84893112e4acd4ca924da350485a
nichmihai <nichmihai4@gmail.com>
2024-09-14 10:50:05 -0400

    adding HelloWorldBean and updating properties

--- src/main/java/com/nichmihai/restapi/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/nichmihai/restapi/restful_web_services/helloworld/HelloWorldController.java
@@ -1,14 +1,24 @@
 package com.nichmihai.restapi.restful_web_services.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String str) {
+        return new HelloWorldBean(String.format("Hello World, %s!", str));
+    }
+
+
 }
